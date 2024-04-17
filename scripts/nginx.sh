2bace6e97572f198dee9974deef0b2db023e60a7
dazai-osamu-111 <nguyentheduc9alacve@gmail.com>
2024-04-18 01:19:01 +0700

    new nginx.conf

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,13 +1,24 @@
 
+# #!/usr/bin/bash
+
+# sudo systemctl daemon-reload
+# sudo rm -f /etc/nginx/sites-enabled/default
+
+# sudo cp /home/ubuntu/bus_backend/nginx/nginx.conf /etc/nginx/sites-available
+# sudo ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled/nginx.conf
+# #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+# sudo nginx -t
+# sudo gpasswd -a www-data ubuntu
+# sudo systemctl restart nginx
+
 #!/usr/bin/bash
 
-sudo systemctl daemon-reload
-sudo rm -f /etc/nginx/sites-enabled/default
+sudo systemctl daemon-reload || exit 1
+sudo rm -f /etc/nginx/sites-enabled/default || exit 1
 
-sudo cp /home/ubuntu/bus_backend/nginx/nginx.conf /etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-#sudo nginx -t
-sudo gpasswd -a www-data ubuntu
-sudo systemctl restart nginx
+sudo cp /home/ubuntu/bus_backend/nginx/nginx.conf /etc/nginx/sites-available/nginx.conf || exit 1
+sudo ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/nginx.conf || exit 1
+
+sudo nginx -t && sudo systemctl restart nginx || exit 1
+sudo gpasswd -a www-data ubuntu || exit 1
 
