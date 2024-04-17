c525f2a9c42761477c52665c9459a9d97b377072
dazai-osamu-111 <nguyentheduc9alacve@gmail.com>
2024-04-18 01:06:09 +0700

    quynh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/bus_backend/nginx/nginx.conf /etc/nginx/sites-available/bus_backend
-sudo ln -s /etc/nginx/sites-available/bus_backend /etc/nginx/sites-enabled/bus_backend
+sudo cp /home/ubuntu/bus_backend/nginx/nginx.conf /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
